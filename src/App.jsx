152789f5c2315dbf8717358d7ed97da4b531bdb1
Jayden Huang <jhuang.study@gmail.com>
2024-11-16 13:08:59 +1100

    Funny change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.js</code> and do something cool.
+          This Website works but since it's been clicked by Ckwok herself I hope it doesn't.
         </p>
         <a
           className="App-link"
